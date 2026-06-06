61d84402bae4942596e5a4d649880799f7e76c52
ict-25 <ict32641@gmail.com>
2026-06-06 01:18:10 +0100

    Add UserRoles constants

---
+++ AlmaERP.Module.Common/Constants/UserRoles.cs
@@ -0,0 +1,13 @@
+namespace AlmaERP.Module.Common.Constants
+{
+    /// <summary>
+    /// User role definitions for role-based access control
+    /// </summary>
+    public static class UserRoles
+    {
+        public const string Administrator = "Administrator";
+        public const string RH = "RH";
+        public const string Manager = "Manager";
+        public const string User = "User";
+    }
+}
